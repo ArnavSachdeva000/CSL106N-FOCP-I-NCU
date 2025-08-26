431d8d36f5454b78dcab8599324ba6ef9e7d54db
Arnav Sachdeva <arnavsachdeva@proton.me>
2025-08-26 17:36:06 +0530

    More Information

--- Class 2/Clean.c
+++ Class 2/Clean.c
@@ -1,4 +1,4 @@
-// This code is for understanding the usage of next line escape sequence in C programming language.
+// This code is for understanding the usage of next line (\n) and tab (\t) escape sequences in C programming language.
 
 #include<stdio.h>
 
@@ -8,7 +8,31 @@ int main()
     printf("\tArnav Sachdeva");
     printf("\nAge");
     printf("\t18 years old");
-    printf("\nOccupation");
-    printf("\tStudent");
+    printf("\nOccupation\t");
+    printf("Student\n");
+    printf("Hobby");
+    printf("\tCoding\n");
+    printf("Country");
+    printf("\tIndia\n");
+    printf("State");
+    printf("\tDelhi\n");
+    printf("City");
+    printf("\tNew Delhi\n");    
+    printf("College");
+    printf("\tThe Northcap University, Haryana\n");
+    printf("Course");
+    printf("\tB.Tech CSE\n");
+    printf("Year");
+    printf("\t1st Year\n");
+    printf("Section");
+    printf("\tCSE-A\n");
+    printf("Group");
+    printf("\t2\n");
     return 0;
 }
+
+// The '\n' escape sequence is used to move the cursor to the next line in the output.
+
+// The '\t' escape sequence is used to insert a horizontal tab space in the output.
+
+// When you run this program, the output will be formatted with proper spacing and new lines as intended.
