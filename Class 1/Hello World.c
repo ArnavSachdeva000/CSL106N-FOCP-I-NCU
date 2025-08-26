b539298acf8eb77944c2784743a621d222c300b9
Arnav Sachdeva <arnavsachdeva@proton.me>
2025-08-26 13:40:36 +0530

    Update

---
+++ Class 1/Hello World.c
@@ -0,0 +1,17 @@
+// This code is for printing "Hello, World"
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, World");
+    return 0;
+}
+
+/*
+To run this code, type the following commands in your terminal:
+gcc "Hello World.c" -o "Hello World.exe"
+.\"Hello World.exe"
+
+Make sure to include the quotes if your file name contains spaces.
+*/
---
+++ Class 2/Next Line.c
@@ -0,0 +1,12 @@
+// This code is for printing "Hello World" with \n and \t
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello\n1234\tWorld");
+    printf("\nnew line");
+    printf(" new line");
+    printf("\n new line");
+    return 0;
+}
